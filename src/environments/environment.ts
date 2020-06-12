5192471d9ff7db0d35f4d403568d57480aec13b5
tavssumbe <ssumbe@tavisca.com>
2020-06-13 02:02:44 +0530

    add shareed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  hostUrl: 'http://192.168.0.100:8080/medistores',
   production:false,
-   firebaseConfig : {
+  firebaseConfig : {
     apiKey: "AIzaSyD7CY29tIguM6PTVPLjg7oDvtbIGphPkn8",
     authDomain: "api-project-586589451606.firebaseapp.com",
     databaseURL: "https://api-project-586589451606.firebaseio.com",
