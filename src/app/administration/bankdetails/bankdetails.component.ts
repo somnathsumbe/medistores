f083525d2d5c697abcf399ae83380809c6502174
Amol Karle <Amol.Karle@in.ey.com>
2020-06-27 16:09:31 +0530

    bank details design

--- src/app/administration/bankdetails/bankdetails.component.ts
+++ src/app/administration/bankdetails/bankdetails.component.ts
@@ -1,69 +1,82 @@
-import { Component, OnInit } from "@angular/core";
-import { LoginService } from "src/app/administration/services/login.service";
+import { Component, OnInit } from '@angular/core';
+import { MastersService } from 'src/app/shared/service/masters.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: "app-bankdetails",
-  templateUrl: "./bankdetails.component.html",
-  styleUrls: ["./bankdetails.component.css"],
+  selector: 'app-bankdetails',
+  templateUrl: './bankdetails.component.html',
+  styleUrls: ['./bankdetails.component.css'],
 })
+
 export class BankdetailsComponent implements OnInit {
-  Banks: string[] = [
-    "Allahabad Bank",
-    "Andhra Bank",
-    "Bank of India",
-    "Bank of Baroda",
-    "Bank of Maharashtra",
-    "Canara Bank",
-    "Central Bank of India",
-    "Corporation Bank",
-    "Dena Bank",
-    "Indian Bank",
-    "Indian Overseas Bank",
-    "IDBI Bank",
-    "Oriental Bank of Commerce",
-    "Punjab & Sindh Bank",
-    "Punjab National Bank",
-    "State Bank of India",
-    "Syndicate Bank",
-    "UCO Bank",
-    "Union Bank of India",
-    "United Bank of India",
-    "Vijaya Bank",
-  ];
-  Cityes: any[] = [];
-  bankForm;
-  msgState:boolean;
-  errorMsg:string;
-  constructor(private _login: LoginService) { }
+  public parties: any[] = [];
+  public Cities: any[] = [];
+  public BankList: any[] = [];
+  public bankForm;
+  constructor(
+    private masterService: MastersService,
+    private toastr: ToastrService
+    ) { }
   ngOnInit(): void {
     this.getBankData();
     this.getCityData();
+    this.getPartyData();
   }
-
 
   getBankData() {
-    //   this._login.bankDetailsList().subscribe(data =>{
-    //    this.Banks=data;
-    //    console.log(this.Banks);
-    //  })
+    this.masterService.fetchDetails('/master/bankNames').subscribe(bankdata => {
+      this.BankList = bankdata;
+      console.log(this.BankList);
+    });
   }
+
   getCityData() {
-    this._login.AllBankList().subscribe(city => {
-      this.Cityes = city;
-    })
+    this.masterService.fetchDetails('location?pageNumber=0&maxCount=100').subscribe(CityList => {
+      this.Cities = CityList;
+      console.log(this.Cities);
+    });
+  }
+
+  getPartyData() {
+    this.masterService.fetchDetails('/customer').subscribe(PartyList => {
+      this.parties = PartyList.map((item) => {
+        const obj = {
+          label: item.firmName,
+          value: item.customerId
+        };
+        return obj;
+      });
+      console.log(this.parties);
+    });
   }
 
   bankDetailsData(bankform: any) {
-    this.bankForm=bankform.value;
-    delete this.bankForm.cityId; 
-    console.log(delete this.bankForm.cityId);
-    this.bankForm["city"] = {"cityId" : parseInt(bankform.form.controls.cityId.value)};
-    this._login.saveBankDetails(this.bankForm).subscribe((bankdata)=>{
-      bankdata;
-      if (bankdata) {
-        this.msgState=true;
-        this.errorMsg = "Banks Details  saved successfully"
-      }
-    })
+    this.bankForm = bankform.value;
+    console.log(this.bankForm);
+    // "bankId": null,
+    // "bankName": "SBI - State Bank",
+    // "address": "Kranti Chauk",
+    // "branchCode": "BNK89234",
+    // "ifscCode": "SBIN0701234",
+    // "cityName": "Ahmednagar",
+    // "customerId": 2
+
+    // address: "sdaada"
+    // bankAccount: 12323
+    // bankId: "Allahabad Bank"
+    // branchCode: "asdad"
+    // cityId: "Akola                    "
+    // ifscCode: "asdasda"
+    // partyName: "1"
+    // postCode: 2133223
+
+    //bankId
+    console.log(this.bankForm);
+    // this.masterService.addEditDetails('bank', this.bankForm).subscribe((bankdata) => {
+    //   if (bankdata) {
+    //     this.toastr.success('Banks Details  saved successfully');
+    //     bankform.reset();
+    //   }
+    // });
   }
 }
--- src/app/shared/service/masters.service.ts
+++ src/app/shared/service/masters.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -7,17 +7,17 @@ import { environment } from 'src/environments/environment';
 })
 export class MastersService {
 
-  constructor(public http: HttpClient) {}
+  constructor( public http: HttpClient ) {}
 
   // Get method to fetch the list
   fetchDetails(url) {
-    const serviceURL = `${environment.hostUrl}/${url}`
+    const serviceURL = `${environment.hostUrl}/${url}`;
     return this.http.get < any > (serviceURL);
   }
 
   // add/edit new Information
   addEditDetails(url, body) {
-    const serviceURL = `${environment.hostUrl}/${url}`
+    const serviceURL = `${environment.hostUrl}/${url}`;
     return this.http.post <any> (serviceURL, body);
   }
 }
